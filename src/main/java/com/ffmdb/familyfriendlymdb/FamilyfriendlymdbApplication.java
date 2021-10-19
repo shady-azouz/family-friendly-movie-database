688ecc95de33e9cc862c82b1957424a15fc14862
shady-azouz <shady.azouz@gmail.com>
2021-10-19 16:40:19 +0200

    Init: add Unversioned Files

---
+++ src/main/java/com/ffmdb/familyfriendlymdb/FamilyfriendlymdbApplication.java
@@ -0,0 +1,13 @@
+package com.ffmdb.familyfriendlymdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyfriendlymdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilyfriendlymdbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ffmdb/familyfriendlymdb/FamilyfriendlymdbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ffmdb.familyfriendlymdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyfriendlymdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
