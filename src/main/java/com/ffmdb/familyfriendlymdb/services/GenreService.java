771d21681d2674ab93300a8d9d5d47ce5beadc96
shady-azouz <shady.azouz@gmail.com>
2021-10-24 17:50:50 +0200

    feat: load Movies and Genres from TMDB at launch

--- src/main/java/com/ffmdb/familyfriendlymdb/services/GenreService.java
+++ src/main/java/com/ffmdb/familyfriendlymdb/services/GenreService.java
@@ -3,13 +3,32 @@ package com.ffmdb.familyfriendlymdb.services;
 import com.ffmdb.familyfriendlymdb.entities.Genre;
 import com.ffmdb.familyfriendlymdb.repositories.GenreRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
+import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
 
 public class GenreService {
     @Autowired
     GenreRepository genreRepository;
+
+    @PostConstruct
+    public void loadGenres() {
+        RestTemplate restTemplate = new RestTemplate();
+        String key = "a8bd7f3d0cff0c86e330f635ea81ce95";
+        ResponseEntity<Genre[]> response =
+                restTemplate.getForEntity(
+                        "https://api.themoviedb.org/3/genre/movie/list"
+                                + "?api_key=" + key
+                                + "&language=en-US",
+                        Genre[].class);
+        Genre[] genres = response.getBody();
+        for(Genre genre : genres){
+            genreRepository.save(genre);
+        }
+    }
 
     public List<Genre> getAllGenres(){
         List<Genre> genres = new ArrayList<>();
--- src/main/java/com/ffmdb/familyfriendlymdb/services/MovieService.java
+++ src/main/java/com/ffmdb/familyfriendlymdb/services/MovieService.java
@@ -3,8 +3,10 @@ package com.ffmdb.familyfriendlymdb.services;
 import com.ffmdb.familyfriendlymdb.entities.Movie;
 import com.ffmdb.familyfriendlymdb.repositories.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
-import javax.persistence.EntityManager;
+import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,10 +14,28 @@ public class MovieService {
     @Autowired
     private MovieRepository movieRepository;
 
+    @PostConstruct
+    public void loadMovies() {
+        RestTemplate restTemplate = new RestTemplate();
+        String key = "a8bd7f3d0cff0c86e330f635ea81ce95";
+        for(int i=1;i<6;i++){
+            ResponseEntity<Movie[]> response =
+                    restTemplate.getForEntity(
+                            "https://api.themoviedb.org/3/movie/top_rated"
+                                    + "?api_key=" + key
+                                    + "?page=" + i
+                                    + "&language=en-US",
+                            Movie[].class);
+            Movie[] movies = response.getBody();
+            for(Movie movie : movies){
+                movieRepository.save(movie);
+            }
+        }
+    }
+
     public List<Movie> getAllMovies(){
         List<Movie> movies = new ArrayList<>();
         movieRepository.findAll().forEach(movies::add);
         return movies;
     }
-//    public
 }
